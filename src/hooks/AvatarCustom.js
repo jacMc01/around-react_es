21b0325c2c221d908e9e2db8c9d63f1ed7623336
Jacqueline Mckay H <jacquelinemc@outlook.es>
2023-03-30 18:20:53 -0600

    hotfix: corrections applied

--- src/hooks/AvatarCustom.js
+++ src/hooks/AvatarCustom.js
@@ -1,43 +1,43 @@
 import {useEffect, useState} from "react";
-import api from "../components/Api";
+import api from "../utils/api";
 
 
 export function AvatarCustom(){
-    const [isAvatar, setIsAvatar] = useState("");
+  const [isAvatar, setIsAvatar] = useState("");
 
-    useEffect(() => {
-        const fetchAvatar = async () => {
-            try {
-                const response = await api.get("users/me", {
-                    headers: {
-                        authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
-                    }
-                });
-                const avatar = response.data.avatar;
-                setIsAvatar(avatar);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchAvatar();
-    }, []);
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      try {
+        const response = await api.get("users/me", {
+          headers: {
+            authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
+          }
+        });
+        const avatar = response.data.avatar;
+        setIsAvatar(avatar);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAvatar();
+  }, []);
 
-    const handleSubmitAvatar = async (e) => {
-        e.preventDefault();
-        try{
-            const response = await api.patch("users/me/avatar", {isAvatar: e.target["popup1__name"].value}, {
-                headers: {
-                    authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
-                    "Content-Type": "application/json"
-                },
-            });
-            const avatarSrc = response.data.isAvatar;
-            setIsAvatar(avatarSrc);
+  const handleSubmitAvatar = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await api.patch("users/me/avatar", {isAvatar: e.target["popup1__name"].value}, {
+        headers: {
+          authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
+          "Content-Type": "application/json"
+        },
+      });
+      const avatarSrc = response.data.isAvatar;
+      setIsAvatar(avatarSrc);
 
-        } catch (error) {
-            console.log(error);
-        }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return {isAvatar, handleSubmitAvatar}
+  return {isAvatar, handleSubmitAvatar}
 }
--- src/hooks/ContextoProvider.js
+++ src/hooks/ContextoProvider.js
@@ -1,129 +1,129 @@
 import React, {useEffect, useState} from "react";
 import { useContext } from "react";
-import Api from "../components/Api";
+import api from "../utils/api";
 
 export const contexto = React.createContext();
 
 export function useCards(){
-    const context = useContext(contexto);
-    if(!context){
-        throw new Error("useCards must be used within a ContextoProvider");
-    }
-    return context;
+  const context = useContext(contexto);
+  if(!context){
+    throw new Error("useCards must be used within a ContextoProvider");
+  }
+  return context;
 }
 
 
 export function ContextoProvider({children}) {
-    const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([]);
 
-    useEffect(() => {
-        fetchCard();
-    }, []);
+  useEffect(() => {
+    fetchCard();
+  }, []);
 
-    const fetchCard = async () => {
-        try {
-            const response = await Api.get("cards", {
-                headers: {
-                    authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
-                }
-            });
-            const cards = response.data;
-            setCards(cards);
-        } catch (error) {
-            console.log(error);
+  const fetchCard = async () => {
+    try {
+      const response = await api.get("cards", {
+        headers: {
+          authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
         }
-    };
-
-    const handleSubmitCard = async (e) => {
-        e.preventDefault();
-        try{
-            const response = await Api.post("cards", {name: e.target["popup3__name"].value, link: e.target["popup3__about"].value}, {
-                headers: {
-                    authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
-                    "Content-Type": "application/json"
-                },
-            });
-
-            const newCard = response.data;
-
-            setCards([newCard, ...cards]);
-            //setUpdate(true);
-
-        } catch (error) {
-            console.log(error);
-        }
+      });
+      const cards = response.data;
+      setCards(cards);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const handleSubmitCard = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await api.post("cards", {name: e.target["popup3__name"].value, link: e.target["popup3__about"].value}, {
+        headers: {
+          authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
+          "Content-Type": "application/json"
+        },
+      });
 
-    const handleDeleteCard = async (event) => {
-        try {
-            const cardId = event.target.getAttribute('data-card-id');
-            await Api.delete(`cards/${cardId}`, {
-                headers: {
-                    authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
-                }
-            });
-            const newCards = cards.filter((card) => card._id !== cardId);
-            setCards(newCards);
-        } catch (error) {
-            console.log(error);
-        }
+      const newCard = response.data;
+
+      setCards([newCard, ...cards]);
+      //setUpdate(true);
+
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    const handleLikeCard = async (event) => {
-        const imgElement = event.target;
 
-        const cardId = event.target.getAttribute('data-card-id');
-        const userId = event.target.getAttribute('data-user-id');
-
-        if (imgElement.src.includes("heart_black")) {
-            try {
-                const send_str_url = `cards/likes/${cardId}`
-                await Api.delete(send_str_url, {
-                    headers: {
-                        Authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
-                    }
-                });
-            } catch (error) {
-                console.log(error);
-            }
+  const handleDeleteCard = async (event) => {
+    try {
+      const cardId = event.target.getAttribute('data-card-id');
+      await api.delete(`cards/${cardId}`, {
+        headers: {
+          authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d"
         }
-        else {
-            try{
-                const send_str_url = `cards/likes/${cardId}`
-                await Api.put(send_str_url,{}, {
-                    headers: {
-                        Authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
-                    }
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        //refrescar todo
-        fetchCard();
-
-        const newCards = cards.map(card => {
-            if(card !== cardId){
-                return card
-            }
-            return {...card, likes: card.likes.filter(like => {
-                return like._id !== userId
-            })}
-        })
-        setCards(newCards);
+      });
+      const newCards = cards.filter((card) => card._id !== cardId);
+      setCards(newCards);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    const valorDelContexto = {
-        cards,
-        handleSubmitCard,
-        handleDeleteCard,
-        handleLikeCard
+  const handleLikeCard = async (event) => {
+    const imgElement = event.target;
+
+    const cardId = event.target.getAttribute('data-card-id');
+    const userId = event.target.getAttribute('data-user-id');
+
+    if (imgElement.src.includes("heart_black")) {
+      try {
+        const send_str_url = `cards/likes/${cardId}`
+        await api.delete(send_str_url, {
+          headers: {
+            Authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
+    else {
+      try{
+        const send_str_url = `cards/likes/${cardId}`
+        await api.put(send_str_url,{}, {
+          headers: {
+            Authorization: "716b8afb-3113-4c1d-98fb-541a60ec168d",
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    //refrescar todo
+    fetchCard();
 
-    return(
-        <contexto.Provider value={valorDelContexto}>
-            {children}
-        </contexto.Provider>
-    )
+    const newCards = cards.map(card => {
+      if(card !== cardId){
+        return card
+      }
+      return {...card, likes: card.likes.filter(like => {
+        return like._id !== userId
+      })}
+    })
+    setCards(newCards);
+  }
+
+  const valorDelContexto = {
+    cards,
+    handleSubmitCard,
+    handleDeleteCard,
+    handleLikeCard
+  }
+
+  return(
+    <contexto.Provider value={valorDelContexto}>
+      {children}
+    </contexto.Provider>
+  )
 }
